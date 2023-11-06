df2325d0c3ae040802f6e71341abc945714eabcc
Somanshu Rajak <120083942+somanshur5976@users.noreply.github.com>
2023-11-06 22:02:34 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee{  
+    public static void main(String[] args){ 
+   String name="John";
+   int empid=14;
+    double salary=150000; 
+double Annual=1800000;
+    System.out.println("Name :"+name);
+   System.out.println("Employee id :"+empid); 
+    System.out.println("Salary :"+salary); 
+ System.out.println("Annual Salary :"+Annual); 
+    }  
+    }  
